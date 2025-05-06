7dac5e6cc9f150ed7f0051e5e2563690a2189dd3
Jonathan Silva <jonathanlive@outlook.com>
2025-05-05 23:39:46 -0300

    Adicionando logs na execucao do crew

--- fastapi_server/brasileirao_connector.py
+++ fastapi_server/brasileirao_connector.py
@@ -6,9 +6,11 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from brasileirao_agent.crew import BrasileiraoAgent
 
 def executar_crew(pergunta: str,sender: str) -> str:
+    print(f"[🎯 CREW] Iniciando análise para pergunta: {pergunta} | Usuário: {sender}")
     inputs = { "pergunta": pergunta }
     try:
         output = BrasileiraoAgent().crew().kickoff(inputs=inputs)
+        print(f"[📤 RESPOSTA FINAL] {output}")
         return str(output)
     except Exception as e:
         return f"⚠️ Erro ao rodar Crew: {e}"
