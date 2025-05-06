64f1ad8ced2bce079f2429de5a828e882be0738e
Jonathan Silva <jonathanlive@outlook.com>
2025-05-06 16:55:14 -0300

    Ajustes na porta do BOT

--- index.cjs
+++ index.cjs
@@ -306,7 +306,7 @@ app.get('/qrcode', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🛡️ Servidor Express rodando na porta ${PORT}`);
 });
